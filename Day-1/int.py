0184db832416c74a6a1b0e0a44843efb987b1f8d
Dipendra <rayamajhidipendra702@gmail.com>
2026-04-26 20:14:02 +0545

    performed some int calucation with abs

---
+++ Day-1/int.py
@@ -0,0 +1,14 @@
+num1 = 5
+num2 = 3
+
+# addition
+result1 = num1 + num2
+print("addition is", result1)
+
+# remainder
+result2 = num1 % num2
+print("remainder is", result2)
+
+#absolute value
+result3 = abs(-6)
+print("absolute value is", result3)
