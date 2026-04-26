c436929e335728b291f979ae8b23d4e365e500ef
Dipendra <rayamajhidipendra702@gmail.com>
2026-04-26 20:11:13 +0545

    just perfom some badmas on float

---
+++ Day-1/float.py
@@ -0,0 +1,22 @@
+num1 = 3.5
+num2 = 2.5
+
+# addition
+result1 = num1 + num2
+print("addition is", result1)
+
+# subtraction
+result2 = num1 - num2
+print("subtraction is", result2)
+
+# multiplication
+result3 = num1 * num2
+print("multiplication is", result3)
+
+# division
+result4 = num1 / num2
+print("division is", result4)
+
+# round
+result5 = round(3.1457374683, 2)
+print("rounded number is", result5)
