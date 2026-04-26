acde9600145531c72292426da42a322c84c60da5
Dipendra <rayamajhidipendra702@gmail.com>
2026-04-26 20:06:53 +0545

    learned about substring and strip in python

---
+++ Day-1/strip.py
@@ -0,0 +1,8 @@
+# text = " some spaces left around at both sides  "
+# strip_text = text.strip()
+# print ("striped text is", strip_text)
+
+text = "Hello this is dipendra rayamahi"
+substring = "dipendra"
+if substring in text:
+    print("substring is present in the text")
