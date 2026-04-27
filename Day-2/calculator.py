e52171a0cda7966ce89ea060d9891e55f7002c67
Dipendra <rayamajhidipendra702@gmail.com>
2026-04-27 16:28:26 +0545

    learning about the function and modulatrity in python

---
+++ Day-2/calculator.py
@@ -0,0 +1,16 @@
+def add(num1, num2):
+    sum = num1 + num2 
+    return sum
+
+def sub(num1, num2):
+    sub = num1 - num2
+    return sub
+
+def multi(num1, num2):
+    mul=num1 * num2
+    return mul
+
+print(add(5,2))
+print(sub(5,2))
+print(multi(5,2))
+  
