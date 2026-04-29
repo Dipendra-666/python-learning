8cef2dbf625a384f846600b8ab910c19928ec88c
Dipendra <rayamajhidipendra702@gmail.com>
2026-04-29 13:00:41 +0545

    sorted and slicing concept

--- Day-3/list.py
+++ Day-3/list.py
@@ -1,9 +1,14 @@
 #list example 
 
 s3_bucket_list = ["dipendra-bucket-1", "dipendra-bucket-2", "dipendra-bucket-3"]
-s3_bucket_list.append("dipendra-bucket-4") # this will add a new bucket to the list
-s3_bucket_list.remove("dipendra-bucket-1") # this will remove the bucket from the list
-print("list of the buskets are:", s3_bucket_list)
+# s3_bucket_list.append("dipendra-bucket-4") # this will add a new bucket to the list
+# s3_bucket_list.remove("dipendra-bucket-1") # this will remove the bucket from the list
+# print("list of the buskets are:", s3_bucket_list)
+print(s3_bucket_list[0:2]) # slicing concept in list 
+
+list_number = [1,4,3,5,6]
+list_number.sort()
+print("sorted list is:", list_number)
 
 #tuple example 
 
