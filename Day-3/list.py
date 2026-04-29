0cb4b3f5622f8f1152c61f87fb8ac250d40a2434
Dipendra <rayamajhidipendra702@gmail.com>
2026-04-29 12:43:39 +0545

    changed the value in list

--- Day-3/list.py
+++ Day-3/list.py
@@ -1,11 +1,13 @@
 #list example 
 
-# s3_bucket_list = ["dipendra-bucket-1", "dipendra-bucket-2", "dipendra-bucket-3"]
-# print("list of the buskets are:", s3_bucket_list)
+s3_bucket_list = ["dipendra-bucket-1", "dipendra-bucket-2", "dipendra-bucket-3"]
+s3_bucket_list.append("dipendra-bucket-4") # this will add a new bucket to the list
+s3_bucket_list.remove("dipendra-bucket-1") # this will remove the bucket from the list
+print("list of the buskets are:", s3_bucket_list)
 
 #tuple example 
 
-s3_bucket_list = ("dipendra-bucket-1", "dipendra-bucket-2", "dipendra-bucket-3")
-print("tuple of the buskets are:", s3_bucket_list)
-s3_bucket_list.append("dipendra-bucket-4") # this will give an error because tuple is immutable
-s3_bucket_list.remove("dipendra-bucket-1") # this will give an error because tuple is immutable
+# s3_bucket_list = ("dipendra-bucket-1", "dipendra-bucket-2", "dipendra-bucket-3")
+# print("tuple of the buskets are:", s3_bucket_list)
+# s3_bucket_list.append("dipendra-bucket-4") # this will give an error because tuple is immutable
+# s3_bucket_list.remove("dipendra-bucket-1") # this will give an error because tuple is immutable
