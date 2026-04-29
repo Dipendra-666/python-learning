551e3c2f39d381ba86bfe81ff6cfb1608c93bc02
Dipendra <rayamajhidipendra702@gmail.com>
2026-04-29 19:44:57 +0545

    creating a multiple dictionary within a list

--- Day-5/dictionary.py
+++ Day-5/dictionary.py
@@ -1,8 +1,26 @@
-student_info = {
-    "name": "Dipe",
-    "age": 56,
-    "courses": ["Python", "Data Science", "Machine Learning"],
-    "is_enrolled": True
-}
+# student_info = {
+#     "name": "Dipe",
+#     "age": 56,
+#     "courses": ["Python", "Data Science", "Machine Learning"],
+#     "is_enrolled": True
+# }
 
-print(student_info["courses"])
+# print(student_info["courses"])
+
+# let's write three instance type of dictionary 
+
+instance_type = [
+    {
+        "id": "instance-1",
+        "type":"t2.micro"
+    },
+    {
+        "name": "instance-2",
+        "type": "t2.medium"
+    },
+    {
+        "name": "instance-3",
+        "type": "t2.large"
+    }
+]
+print(instance_type[1]["type"])
