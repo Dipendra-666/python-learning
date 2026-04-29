98d7361469e7cd1ea229c5d204f3bec976d91cae
Dipendra <rayamajhidipendra702@gmail.com>
2026-04-29 12:25:10 +0545

    practise tuple

--- Day-3/list.py
+++ Day-3/list.py
@@ -1,4 +1,9 @@
 #list example 
 
-s3_bucket_list = ["dipendra-bucket-1", "dipendra-bucket-2", "dipendra-bucket-3"]
-print("list of the buskets are:", s3_bucket_list)
+# s3_bucket_list = ["dipendra-bucket-1", "dipendra-bucket-2", "dipendra-bucket-3"]
+# print("list of the buskets are:", s3_bucket_list)
+
+#tuple example 
+
+s3_bucket_list = ("dipendra-bucket-1", "dipendra-bucket-2", "dipendra-bucket-3")
+print("tuple of the buskets are:", s3_bucket_list)
