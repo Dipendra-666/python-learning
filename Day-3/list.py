2f4f77077d61341018da6c2bd6631fdd9b249b2d
Dipendra <rayamajhidipendra702@gmail.com>
2026-04-29 12:40:57 +0545

    changing the value in tuple which give an error

--- Day-3/list.py
+++ Day-3/list.py
@@ -7,3 +7,5 @@
 
 s3_bucket_list = ("dipendra-bucket-1", "dipendra-bucket-2", "dipendra-bucket-3")
 print("tuple of the buskets are:", s3_bucket_list)
+s3_bucket_list.append("dipendra-bucket-4") # this will give an error because tuple is immutable
+s3_bucket_list.remove("dipendra-bucket-1") # this will give an error because tuple is immutable
