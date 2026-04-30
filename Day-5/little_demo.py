32343417f712846c804ad24813f5b4aa5144bf02
Dipendra <rayamajhidipendra702@gmail.com>
2026-04-30 18:57:55 +0545

    just did a little work to optimize it

--- Day-5/little_demo.py
+++ Day-5/little_demo.py
@@ -1,3 +1,4 @@
+# a little demo to show how to use requests library to get data from github api and print the login of the user who created the pull request.
 import requests
 
 response = requests.get("https://api.github.com/repos/kubernetes/kubernetes/pulls")
